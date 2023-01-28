a237dce313a84478859cab65a00c14e440fefa94
shawnliu <liuyihsuan1012@gmail.com>
2023-01-28 17:10:50 +0800

    set btn style

--- src/components/ui/header/index.tsx
+++ src/components/ui/header/index.tsx
@@ -5,11 +5,11 @@ const Header = () =>
 {
     return (
         <div className=" bg-yellow-50 flex justify-between px-35 py-5">
-        <div className="logo">
+        <div className="flex flex-row items-center">
             <img className="img" src={require("./logo.png")} alt=""></img>
-            <span className="title">印石這樣刻崗好</span>
+            <span className="text-red-700 text-4xl">印石這樣刻崗好</span>
         </div>
-        <div className="btn-part">
+        <div className="flex flex-row items-center">
             <button className="btn">登入</button>
             <button className="btn">註冊</button>
         </div>
