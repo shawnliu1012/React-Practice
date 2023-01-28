0753543f89f1b99ea6c07afc65e95b774fc46412
shawnliu <liuyihsuan1012@gmail.com>
2023-01-28 17:19:46 +0800

    設定padding 完成完成nav

--- src/components/ui/header/index.tsx
+++ src/components/ui/header/index.tsx
@@ -4,14 +4,14 @@ import 'App.css';
 const Header = () =>
 {
     return (
-        <div className=" bg-yellow-50 flex justify-between px-35 py-5">
+        <div className=" bg-yellow-50 flex justify-between px-9 py-5">
         <div className="flex flex-row items-center">
-            <img className="img" src={require("./logo.png")} alt=""></img>
+            <img className="" src={require("./logo.png")} alt=""></img>
             <span className="text-red-700 text-4xl">印石這樣刻崗好</span>
         </div>
         <div className="flex flex-row items-center">
-            <button className="btn">登入</button>
-            <button className="btn">註冊</button>
+            <button className="btn mx-5 text-sm">登入</button>
+            <button className="btn mx-5 text-sm">註冊</button>
         </div>
     </div>
     );
