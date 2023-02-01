0655057c745d82f6bc6e55e02a367be9a1c13621
shawnliu <liuyihsuan1012@gmail.com>
2023-02-01 09:36:52 +0800

    設定分類內容 hover底線需再調整

--- src/components/ui/main/index.tsx
+++ src/components/ui/main/index.tsx
@@ -4,12 +4,15 @@ import 'App.css';
 const Main = () => {
     return (
         <div className="main">
-        <div className="flex justify-center">
-            <div className="mx-11 my-3.5 text-4xl hover:text-blue-500 cursor-pointer">假文</div>
-            <div className="mx-11 my-3.5 text-4xl hover:text-blue-500 cursor-pointer">假文</div>
-            <div className="mx-11 my-3.5 text-4xl hover:text-blue-500 cursor-pointer">假文</div>
-            <div className="mx-11 my-3.5 text-4xl hover:text-blue-500 cursor-pointer">假文</div>
-            <div className="mx-11 my-3.5 text-4xl hover:text-blue-500 cursor-pointer">假文</div>
+        <div className="flex justify-center my-4">
+            <div className="mx-11 my-4 text-xl hover:text-blue-500 hover:border-b-4 border-gray-600 cursor-pointer">人員介紹</div>
+            <div className="mx-11 my-4 text-xl hover:text-blue-500 hover:border-b-4 border-gray-600 cursor-pointer">作品集</div>
+            <div className="mx-11 my-4 text-xl hover:text-blue-500 hover:border-b-4 border-gray-600 cursor-pointer">年度活動</div>
+            <div className="mx-11 my-4 text-xl hover:text-blue-500 hover:border-b-4 border-gray-600 cursor-pointer">公司介紹</div>
+            <div className="mx-11 my-4 text-xl hover:text-blue-500 hover:border-b-4 border-gray-600 cursor-pointer">大事紀</div>
+            <div className="mx-11 my-4 text-xl hover:text-blue-500 hover:border-b-4 border-gray-600 cursor-pointer">會員專區</div>
+            <div className="mx-11 my-4 text-xl hover:text-blue-500 hover:border-b-4 border-gray-600 cursor-pointer">教育訓練</div>
+            <div className="mx-11 my-4 text-xl hover:text-blue-500 hover:border-b-4 border-gray-600 cursor-pointer">留言區</div>
         </div>
         <img className="container" src={require("./系統更新中.png")} alt="" ></img>
     </div>
